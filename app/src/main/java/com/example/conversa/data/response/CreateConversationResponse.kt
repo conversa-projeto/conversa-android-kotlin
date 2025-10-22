f348d05b524796409e040ede844d00e914af91cb
DanielAraujo-dac <danieldearaujocosta@gmail.com>
2025-10-21 21:25:03 -0400

    remove fontes antigos

--- app/src/main/java/com/example/conversa/data/response/CreateConversationResponse.kt
+++
@@ -1,9 +0,0 @@
-// com.example.conversa.data.response/CreateConversationResponse.kt
-package com.example.conversa.data.response
-
-data class CreateConversationResponse(
-    val id: Int,
-    val descricao: String,
-    val inserida: String,
-    val tipo: Int
-)
